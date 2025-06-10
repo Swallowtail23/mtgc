cfb3730aae6d42dd1c67d0ad1b48323d38764fac
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-10 15:29:28 +1000

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,14 @@
 <?php
 // Basic bootstrap for tests
-$logfile = sys_get_temp_dir() . '/phpunit.log';
-$loglevelini = 0;
+$GLOBALS['logfile'] = sys_get_temp_dir() . '/phpunit.log';
+$GLOBALS['loglevelini'] = 0;
+
 $db = new class {
     public function real_escape_string($str) { return $str; }
 };
+
 $bracketsInNames = [];
 $importLinestoIgnore = [];
+
 require __DIR__ . '/../classes/message.class.php';
 require __DIR__ . '/../includes/functions.php';
