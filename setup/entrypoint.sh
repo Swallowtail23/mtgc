ad5e376371c839ce06dce4052ea94820d983e58a
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-11 20:51:03 +1000

    Update entrypoint.sh

--- setup/entrypoint.sh
+++ setup/entrypoint.sh
@@ -3,7 +3,7 @@
 # Ensure log file exists and has correct ownership
 LOG_FILE="/var/log/mtg/mtgapp.log"
 if [ ! -f "$LOG_FILE" ]; then
-touch "$LOG_FILE"
+    touch "$LOG_FILE"
 fi
 chown www-data:www-data "$LOG_FILE"
 
