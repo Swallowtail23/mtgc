446845f27620ca1779b9c305a23459ef29d3b631
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-11 23:30:57 +1000

    Update sessionname_template.php

--- includes/sessionname_template.php
+++ includes/sessionname_template.php
@@ -1,6 +1,6 @@
 <?php
 
-// Either just use the default below, or set your own in a copy of this file named sessionname.php
+// Either just use the default below, or set your own in a copy of this file named sessionname.local.php
 
 function startCustomSession() {
     ini_set('session.name', 'Change-to-Your-Own-Value_2024');
