29268ce2b8bf75aff1ed7395b9ec5cd57c4534dc
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-11 23:44:13 +1000

    Update googlefonts.php

--- includes/googlefonts.php
+++ includes/googlefonts.php
@@ -1,23 +1,19 @@
 <?php 
-/* Version:     1.0
-    Date:       17/10/16
+/* Version:     1.1
+    Date:       11/06/25
     Name:       googlefonts.php
     Purpose:    PHP script to link to Google Roboto fonts
     Notes:      {none}
         
     1.0
                 Initial version
+    1.1         11/06/25
+                Removed html output after ?> (using echo)
  */
 
 if (__FILE__ == $_SERVER['PHP_SELF']) :
 die('Direct access prohibited');
 endif;
-echo <<<HTML
-<link 
-    href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic%7CRoboto:400,300,300italic,500' 
-    rel='stylesheet' 
-    type='text/css'>
-<link 
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    rel="stylesheet" />
-HTML;
+
+echo "<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300italic%7CRoboto:400,300,300italic,500' rel='stylesheet' type='text/css'>\n";
+echo "<link href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' rel='stylesheet' />\n";
