e10327232f9f1965ff0d7ecc85aff16ce76a931b
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-11 23:46:16 +1000

    Update analyticstracking.php

--- includes/analyticstracking.php
+++ includes/analyticstracking.php
@@ -1,9 +1,16 @@
+<?php
+if (__FILE__ == $_SERVER['PHP_SELF']) :
+    die('Direct access prohibited');
+endif;
+
+echo <<<HTML
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-TLT1JVWWQR"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'G-TLT1JVWWQR');
 </script>
+
+HTML;
