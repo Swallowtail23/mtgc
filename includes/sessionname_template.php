4a0a47e30230d3a5559f08145322a23e8dcbfc43
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-13 16:29:50 +1000

    Update sessionname_template.php

--- includes/sessionname_template.php
+++ includes/sessionname_template.php
@@ -2,6 +2,18 @@
 
 // Either just use the default below, or set your own in a copy of this file named sessionname.local.php
 
+$isHttps = (
+    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')
+    || ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') === 'https'
+);
+
+// override the default if needed
+if ($isHttps) {
+    ini_set('session.cookie_secure', '1');
+} else {
+    ini_set('session.cookie_secure', '0');
+}
+
 function startCustomSession() {
     ini_set('session.name', 'Change-to-Your-Own-Value_2024');
     session_start();
