9a035d45e1b1cd99689ac0296b280f83d074b4e1
Swallowtail23 <Swallowtail23@users.noreply.github.com>
2025-06-13 13:11:58 +1000

    Update ini.php

--- includes/ini.php
+++ includes/ini.php
@@ -68,60 +68,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\PHPMailer;
 
 // DEFAULT SETTINGS
-$defaults = [
-    'general' => [
-        'title'       => 'MtG collection',
-        'tier'        => 'prod',                      // either 'dev' or 'prod'
-        'ImgLocation' => '/mnt/data/cardimg/',        // ensure web server can write here
-        'Logfile'     => '/var/log/mtg/mtgapp.log',   // ensure web server can write here
-        'Loglevel'    => 3,                           // see admin pages
-        'Timezone'    => 'Australia/Brisbane',
-        'Locale'      => 'en_US',
-        'Copyright'   => 'Simon Wilson - 2025',
-        'URL'         => 'http://localhost:8080',     // update to your actual URL
-    ],
-
-    'database' => [
-        'DBServer' => 'db',
-        'DBUser'   => 'mtg',
-        'DBPass'   => 'mtgpass',
-        'DBName'   => 'mtg',
-    ],
-
-    'security' => [
-        'AdminIP'              => '',
-        'Badloginlimit'        => 5,
-        'Turnstile'            => 'disabled',
-        'Turnstile_site_key'   => 'xxxxxx',
-        'Turnstile_secret_key' => 'xxxxxx',
-        'TrustDuration'        => 7,                // days
-    ],
-
-    'fx' => [
-        'FreecurrencyAPI' => 'disabled',            // If null or disabled, fx conversion is disabled
-        'FreecurrencyURL' => 'https://api.freecurrencyapi.com/v1/latest?apikey=',
-        'TargetCurrency'  => 'aud',
-    ],
-
-    'email' => [
-        'Email'          => 'disabled',
-        'ServerEmail'    => 'no_reply@your-mtg-site-url.com',
-        'AdminEmail'     => 'youremail@youremail.com',
-        'SMTPDebug'      => SMTP::DEBUG_OFF,
-        'Host'           => 'localhost',
-        'SMTPAuth'       => false,
-        'Username'       => '',
-        'Password'       => '',
-        'SMTPSecure'     => PHPMailer::ENCRYPTION_SMTPS,
-        'Port'           => 25,
-    ],
-
-    'comments' => [
-        'Disqus'        => 'disabled',
-        'DisqusDevURL'  => 'https://dev-url-here.disqus.com',
-        'DisqusProdURL' => 'https://prod-url-here.disqus.com',
-    ],
-];
+require __DIR__ . '/config_defaults.php';
 
 //Set error reporting based on ini file's dev setting
 $ini = new INI("/opt/mtg/mtg_new.ini");
