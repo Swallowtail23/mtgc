de745a94f7499a42159ad3a0abf1f65eebe35194
Simon Wilson <simon@simonandkate.net>
2025-11-26 09:04:45 +1100

    Standardise footer.php with Codex

--- includes/footer.php
+++ includes/footer.php
@@ -1,32 +1,33 @@
 <?php
-/* Version:     1.0
-    Date:       17/10/16
-    Name:       footer.php
-    Purpose:    PHP script to display footer
-    Notes:      {none}
-        
-    1.0
-                Initial version
- */
 
-if (__FILE__ == $_SERVER['PHP_SELF']) :
-die('Direct access prohibited');
+/*
+Version:     1.1
+Date:        26/11/25
+Name:        footer.php
+Purpose:     PHP script to display footer
+Notes:       {none}
+
+History:
+    1.0 17/10/16 Initial version
+    1.1 26/11/25 Standard tidy-up
+*/
+
+if (__FILE__ === $_SERVER['PHP_SELF']) :
+    die('Direct access prohibited');
 endif;
+
+$cssValidatorUrl = 'https://jigsaw.w3.org/css-validator/validator?uri='
+    . 'https://www.mtgcollection.info/css/style.css' . $cssver . '.css';
 ?>
 
 <div id="footer">
     <br>
     &copy; <?php echo $copyright;?>
     <br><br>
-    <a href="https://jigsaw.w3.org/css-validator/validator?uri=https://www.mtgcollection.info/css/style.css<?php echo $cssver?>.css">
-                <img
-                src="/images/valid_css3.png"
-                alt="Valid CSS!" />
+    <a href="<?php echo $cssValidatorUrl;?>">
+        <img src="/images/valid_css3.png" alt="Valid CSS!">
     </a><br><br>
     <a href="https://validator.w3.org/check?uri=https://www.mtgcollection.info">
-                <img
-                src="/images/valid_html5.png"
-                alt="Valid HTML5!" />
-            </a>
-    
+        <img src="/images/valid_html5.png" alt="Valid HTML5!">
+    </a>
 </div>
