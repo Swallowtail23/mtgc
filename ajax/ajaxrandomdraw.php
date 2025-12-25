0e72ee7ddb6452d9225b1efdc5b1bef73e388534
Simon Wilson <simon@simonandkate.net>
2025-12-25 13:20:51 +1100

    Improve robustness and XSS safety in random draw

--- ajax/ajaxrandomdraw.php
+++ ajax/ajaxrandomdraw.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Version:     1.8
+Version:     1.9
 Date:        24/12/25
 Name:        ajaxrandomdraw.php
 Purpose:     PHP script to generate random hand draws for decks
@@ -11,8 +11,8 @@ To do:       -
 */
 
 if (defined('INCLUDE_CHECK') && INCLUDE_CHECK === true) :
-    if (!isset($uniquecard_ref)) :
-        // do nothing
+    if (!isset($uniquecard_ref) || !is_array($uniquecard_ref)) :
+        $uniquecard_ref = [];
     endif;
 elseif ($_SERVER['REQUEST_METHOD'] === 'POST') :
     if (file_exists('../includes/sessionname.local.php')) :
@@ -54,15 +54,47 @@ else :
     die('Direct access prohibited');
 endif;
 
+if (!is_array($uniquecard_ref)) :
+    $uniquecard_ref = [];
+endif;
+
+$filtered_refs = [];
+foreach ($uniquecard_ref as $entry) :
+    if (!is_array($entry)) :
+        continue;
+    endif;
+    $filtered_refs[] = $entry;
+endforeach;
+
+$uniquecard_ref = $filtered_refs;
+if (count($uniquecard_ref) < 7) :
+    if (!defined('INCLUDE_CHECK') || INCLUDE_CHECK !== true) :
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Pragma: no-cache');
+        header('Expires: 0');
+    endif;
+    echo "<div class='msg-new error-new'><span>Not enough cards for a random draw</span></div>";
+    exit();
+endif;
+
 $a = array_rand($uniquecard_ref, 7);
 echo "<table>";
 echo "<tr><td>&nbsp;</td></tr>";
 for ($i = 0; $i < 7; $i++) {
-    $cardurl = $uniquecard_ref[$a[$i]]['cardurl'];
-    $imgurl = $uniquecard_ref[$a[$i]]['imageurl'];
-    $name = $uniquecard_ref[$a[$i]]['name'];
-    $id = $uniquecard_ref[$a[$i]]['cardid'];
-    $cardref = $uniquecard_ref[$a[$i]]['cardref'];
+    $cardurl = $uniquecard_ref[$a[$i]]['cardurl'] ?? '';
+    if (!is_string($cardurl) || !preg_match('#^/carddetail\.php\?id=[A-Za-z0-9-]+$#', $cardurl)) :
+        $cardurl = '#';
+    endif;
+    $imgurl = $uniquecard_ref[$a[$i]]['imageurl'] ?? '/images/back.jpg';
+    if (
+        !is_string($imgurl)
+        || !preg_match('#^/(images|cardimg)/[A-Za-z0-9/_\.-]+$#', $imgurl)
+    ) :
+        $imgurl = '/images/back.jpg';
+    endif;
+    $name = $uniquecard_ref[$a[$i]]['name'] ?? '';
+    $id = $uniquecard_ref[$a[$i]]['cardid'] ?? '';
+    $cardref = $uniquecard_ref[$a[$i]]['cardref'] ?? '';
     $layout = $uniquecard_ref[$a[$i]]['layout'] ?? null;
     $f1_type = $uniquecard_ref[$a[$i]]['f1_type'] ?? null;
     $randomref = $i + 1; ?>
@@ -77,17 +109,20 @@ for ($i = 0; $i < 7; $i++) {
     endif;
     ?>
     <div class='<?php echo $hoverclass; ?>' id='<?php echo "random-$randomref";?>'>
-        <a href='<?php echo $cardurl;?>'>
+        <a href='<?php echo htmlspecialchars($cardurl, ENT_QUOTES, 'UTF-8');?>'>
         <img
-            alt='<?php echo $name;?>'
+            alt='<?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8');?>'
             class='deckcardimg'
-            data-cardid="<?php echo $id; ?>"
-            data-front-src="<?php echo $imgurl; ?>"
-            src='<?php echo $imgurl;?>'
+            data-cardid="<?php echo htmlspecialchars($id, ENT_QUOTES, 'UTF-8'); ?>"
+            data-front-src="<?php echo htmlspecialchars($imgurl, ENT_QUOTES, 'UTF-8'); ?>"
+            src='<?php echo htmlspecialchars($imgurl, ENT_QUOTES, 'UTF-8');?>'
         ></a>
     </div> <?php
+    $safeName = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+    $safeId = htmlspecialchars($id, ENT_QUOTES, 'UTF-8');
+    $safeUrl = htmlspecialchars($cardurl, ENT_QUOTES, 'UTF-8');
     echo "<tr><td class='hoverTD'>$randomref: <a class='taphover' id='random-$randomref-taphover' "
-        . "data-cardid='{$id}' href='$cardurl'>"
-        . "$name</a></td></tr>";
+        . "data-cardid='{$safeId}' href='{$safeUrl}'>"
+        . "$safeName</a></td></tr>";
 }
 echo "</table>";
